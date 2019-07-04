ae2615fc33e78249c446989dfc314dc42e6a3ceb
h-tanaka <h-tanaka@sample.com>
2019-07-04 12:34:49 +0900

    演習レビュー2回目

--- exercise/exercise3/script.js
+++ exercise/exercise3/script.js
@@ -6,25 +6,25 @@ if(time >= 0 && time <= 2){
 	//入力値と判定された配列文の出力
 	document.getElementById('hour').textContent = time +'時は' +timeAllocation[0]+ 'です。';
 
-}else if(time >= 3 && time <= 5){
+}else if(time <= 5){
 	document.getElementById('hour').textContent = time +'時は' +timeAllocation[1]+ 'です。';
 
-}else if(time >= 6 && time <= 8){
+}else if(time <= 8){
 	document.getElementById('hour').textContent = time +'時は' +timeAllocation[2]+ 'です。';
 
-}else if(time >= 9 && time <= 11){
+}else if(time <= 11){
 	document.getElementById('hour').textContent = time +'時は' +timeAllocation[3]+ 'です。';
 
-}else if(time >= 12 && time <= 14){
+}else if(time <= 14){
 	document.getElementById('hour').textContent = time +'時は' +timeAllocation[4]+ 'です。';
 
-}else if(time >= 15 && time <= 17){
+}else if(time <= 17){
 	document.getElementById('hour').textContent = time +'時は' +timeAllocation[5]+ 'です。';
 
-}else if(time >= 18 && time <= 20){
+}else if(time <= 20){
 	document.getElementById('hour').textContent = time +'時は' +timeAllocation[6]+ 'です。';
 
-}else if(time >= 21 && time <= 24){
+}else if(time <= 24){
 	document.getElementById('hour').textContent = time +'時は' +timeAllocation[7]+ 'です。';
 //時間以外のすべてはエラーとする
 }else{
