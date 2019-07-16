1eb4ddfcd5cfe1ea60a04e5dedaf9267b0c75a5f
h-tanaka <h-tanaka@sample.com>
2019-07-16 12:05:53 +0900

    演習問題5,6,7 3回目

--- exercise/exercise5/script.js
+++ exercise/exercise5/script.js
@@ -1,13 +1,14 @@
 
-var images = ['','0から始めるには打って付けのプログラミング言語',
-	'少し分かりずらいプログラミング言語、ゲーム制作ならよく使う',
-	'コードがシンプルでわかりやすいプログラミング言語'];
+var images = {'':'',
+	Java:'0から始めるには打って付けのプログラミング言語',
+	C:'少し分かりずらいプログラミング言語、ゲーム制作ならよく使う',
+	Python:'コードがシンプルでわかりやすいプログラミング言語'};
 
-//プルダウンで値を取得し配列からテキストを呼び出し出力
+//プルダウンで値を取得しobjectからテキストを呼び出し出力
 document.getElementById('form').select.onchange = function() {
-	var urlNo = parseInt(document.getElementById('form').select.value);
+	var genre = document.getElementById('form').select.value;
 
-	document.getElementById('content').textContent = images[urlNo];
+	document.getElementById('content').textContent = images[genre];
 
 }
 
--- exercise/exercise6/script.js
+++ exercise/exercise6/script.js
@@ -1,5 +1,3 @@
-//}); $(document).ready(function()はファイルに一つで事足りる複数は必要ない
-
 var search;
 $(document).ready(function(){
 
@@ -9,20 +7,26 @@ $(document).ready(function(){
 	});
 	//閉じるボタンでのモーダルの閉じる処理
 	$('#modal-close').on('click', function(){
-		$('.hidden').css('display','none');
+		model();
 	});
 	//書き込み、入力値取得、の処理
 	$('#kaku').on('click', function(){
 
-		//search = document.getElementById('form').word.value;
+
 		search = $('#form [name=word]').val();
 
 		if(typeof search === "string" && search !== ""){
 			$('#henkou').text(search);
-			$('.hidden').css('display','none');
+			model();
 		}
 		return false;
 
-
 	});
+
+	//同じ処理のメソッド化
+	var model =function(){
+		$('.hidden').css('display','none');
+	}
 });
+
+
