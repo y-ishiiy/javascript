ac71971fcbd6ef895e08321c7561cb3c47d53934
h-tanaka <h-tanaka@sample.com>
2019-07-16 13:12:19 +0900

    演習問題6

--- exercise/exercise6/script.js
+++ exercise/exercise6/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	});
 	//閉じるボタンでのモーダルの閉じる処理
 	$('#modal-close').on('click', function(){
-		model();
+		modal_close();
 	});
 	//書き込み、入力値取得、の処理
 	$('#kaku').on('click', function(){
@@ -17,14 +17,14 @@ $(document).ready(function(){
 
 		if(typeof search === "string" && search !== ""){
 			$('#henkou').text(search);
-			model();
+			modal_close();
 		}
 		return false;
 
 	});
 
-	//同じ処理のメソッド化
-	var model =function(){
+	//モーダルを閉じる処理のメソッド化
+	var modal_close =function(){
 		$('.hidden').css('display','none');
 	}
 });
