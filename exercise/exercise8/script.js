31935a05dccc5ddd7cdc22890c5993ebed6af2a5
h-tanaka <h-tanaka@sample.com>
2019-07-19 18:16:32 +0900

    世界遺産

--- exercise/exercise8/script.js
+++ exercise/exercise8/script.js
@@ -1,50 +1,44 @@
-var genre = ['.wa','.tyu','.yo'];
-
 $(function(){
 	//メインメニュー開閉処理
 	$('.submenu h3').on('click', function(){
 		$(this).next().toggleClass('hidden');
 	});
 	//メニュー追加処理
-	$('#kaku').on('click', function(){
+	$('#menuadd').on('click', function(){
 
-		var cookgenre = parseInt($('#form [name=select]').val());
+		var cookgenre = $('#form [name=select]').val();
 		var cookname = $('#form [name=word]').val();
 
 		let li = $('<li>').text(cookname);
-		$('ul' + genre[cookgenre]).append(li);
-
-		//初回追加時の削除ボタン表示
-		if (typeof cookname === "string" && cookname !== "") {
-			$('.hidden1').css('display','block');
-		}
+		$('ul' + cookgenre).append(li);
 	});
 
 	//削除のジャンルプルダウン切り替え時の削除メニュー一覧表示
-	$('#delpull').change(function() {
-		var cookNo = parseInt($('#formdel [name=selectgenre]').val());
-		$('#delsel option').remove();
-		$('.hidden2').css('display','none');
+	$('#delpullgenre').change(function() {
+		var cookgenre = $('#formdel [name=selectgenre]').val();
+		$('#delpullmenu option').remove();
+		$('.hidden1').css('display','none');
 
-		$.each($(genre[cookNo]+' li'), function (i, elem) {
+		$.each($(cookgenre+' li'), function (i, elem) {
 			var menutext= $(elem).text();
 			option = $('<option>')
 			.val(i)
 			.text(menutext)
 			.prop('selected', 'true');
-			$('#delsel').append(option);
+			$('#delpullmenu').append(option);
 		});
-		if(!isNaN(cookNo)){
-			$('.hidden2').css('display','block');
+		//ジャンル未選択時のプル非表示
+		if(typeof cookgenre === "string" && cookgenre !== ""){
+			$('.hidden1').css('display','block');
 		}
 	});
 	//削除処理
-	$('#kesu').on('click', function(){
-		var cookNo = parseInt($('#formdel [name=selectgenre]').val());
+	$('#delete').on('click', function(){
+		var cookgenre = $('#formdel [name=selectgenre]').val();
 		var menuNo = parseInt($('#formdel [name=menu]').val());
 
-		$(genre[cookNo]+' li').eq(menuNo).remove();
-		$('#delpull').trigger('change');
+		$(cookgenre +' li').eq(menuNo).remove();
+		$('#delpullgenre').trigger('change');
 
 	});
 
